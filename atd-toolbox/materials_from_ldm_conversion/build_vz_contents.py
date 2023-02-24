5d6f4a43be870cf3ee1f7c3921f18b808e9ce968
Frank Hereford <frankhereford@users.noreply.github.com>
2023-02-24 10:29:07 -0600

    get connected

---
+++ atd-toolbox/materials_from_ldm_conversion/build_vz_contents.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv("env")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+DB_SSL_REQUIREMENT = os.getenv("DB_SSL_REQUIREMENT")
+
+def get_pg_connection():
+    """
+    Returns a connection to the Postgres database
+    """
+    return psycopg2.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASS,
+        dbname=DB_NAME,
+        sslmode=DB_SSL_REQUIREMENT,
+        sslrootcert="/root/rds-combined-ca-bundle.pem",
+    )
+
+pg = get_pg_connection()
