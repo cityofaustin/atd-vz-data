8250d4d3b432916606f8ea54a242f875d7a6c69f
Frank Hereford <frankhereford@users.noreply.github.com>
2023-02-24 09:51:08 -0600

    reorder this for debugging

--- atd-toolbox/materials_from_ldm_conversion/create_cris_schema.sql
+++ atd-toolbox/materials_from_ldm_conversion/create_cris_schema.sql
@@ -1,4 +1,4 @@
-drop if exists schema cris cascade;
+drop schema if exists cris cascade;
 create schema cris;
 
 
@@ -220,25 +220,6 @@ CREATE TABLE cris.atd_txdot_crashes (
 ALTER TABLE ONLY cris.atd_txdot_crashes
     ADD CONSTRAINT atd_txdot_crashes_pkey PRIMARY KEY (crash_id);
 
-CREATE INDEX atd_txdot_crashes_apd_confirmed_fatality_index ON cris.atd_txdot_crashes USING btree (apd_confirmed_fatality);
-CREATE INDEX atd_txdot_crashes_austin_full_purpose_index ON cris.atd_txdot_crashes USING btree (austin_full_purpose);
-CREATE INDEX atd_txdot_crashes_case_id_index ON cris.atd_txdot_crashes USING btree (case_id);
-CREATE INDEX atd_txdot_crashes_city_id_index ON cris.atd_txdot_crashes USING btree (city_id);
-CREATE INDEX atd_txdot_crashes_cr3_file_metadata_index ON cris.atd_txdot_crashes USING gin (cr3_file_metadata);
-CREATE INDEX atd_txdot_crashes_cr3_stored_flag_index ON cris.atd_txdot_crashes USING btree (cr3_stored_flag);
-CREATE INDEX atd_txdot_crashes_crash_date_index ON cris.atd_txdot_crashes USING btree (crash_date);
-CREATE INDEX atd_txdot_crashes_crash_fatal_fl_index ON cris.atd_txdot_crashes USING btree (crash_fatal_fl);
-CREATE INDEX atd_txdot_crashes_death_cnt_index ON cris.atd_txdot_crashes USING btree (death_cnt);
-CREATE INDEX atd_txdot_crashes_geocode_provider_index ON cris.atd_txdot_crashes USING btree (geocode_provider);
-CREATE INDEX atd_txdot_crashes_geocode_status_index ON cris.atd_txdot_crashes USING btree (geocode_status);
-CREATE INDEX atd_txdot_crashes_geocoded_index ON cris.atd_txdot_crashes USING btree (geocoded);
-CREATE INDEX atd_txdot_crashes_investigat_agency_id_index ON cris.atd_txdot_crashes USING btree (investigat_agency_id);
-CREATE INDEX atd_txdot_crashes_is_retired_index ON cris.atd_txdot_crashes USING btree (is_retired);
-CREATE INDEX atd_txdot_crashes_original_city_id_index ON cris.atd_txdot_crashes USING btree (original_city_id);
-CREATE INDEX atd_txdot_crashes_position_index ON cris.atd_txdot_crashes USING gist ("position");
-CREATE INDEX atd_txdot_crashes_qa_status_index ON cris.atd_txdot_crashes USING btree (qa_status);
-CREATE INDEX atd_txdot_crashes_sus_serious_injry_cnt_index ON cris.atd_txdot_crashes USING btree (sus_serious_injry_cnt);
-CREATE INDEX atd_txdot_crashes_temp_record_index ON cris.atd_txdot_crashes USING btree (temp_record);
 
 --------------------------------------------------------------------------------
 
@@ -304,17 +285,6 @@ ALTER TABLE ONLY cris.atd_txdot_person ALTER COLUMN person_id SET DEFAULT nextva
 ALTER TABLE ONLY cris.atd_txdot_person
     ADD CONSTRAINT atd_txdot_person_unique UNIQUE (crash_id, unit_nbr, prsn_nbr, prsn_type_id, prsn_occpnt_pos_id);
 
-CREATE INDEX atd_txdot_person_death_cnt_index ON cris.atd_txdot_person USING btree (death_cnt);
-CREATE INDEX atd_txdot_person_is_retired_index ON cris.atd_txdot_person USING btree (is_retired);
-CREATE INDEX atd_txdot_person_person_id_index ON cris.atd_txdot_person USING btree (person_id);
-CREATE INDEX atd_txdot_person_prsn_age_index ON cris.atd_txdot_person USING btree (prsn_age);
-CREATE INDEX atd_txdot_person_prsn_death_date_index ON cris.atd_txdot_person USING btree (prsn_death_date);
-CREATE INDEX atd_txdot_person_prsn_death_time_index ON cris.atd_txdot_person USING btree (prsn_death_time);
-CREATE INDEX atd_txdot_person_prsn_ethnicity_id_index ON cris.atd_txdot_person USING btree (prsn_ethnicity_id);
-CREATE INDEX atd_txdot_person_prsn_gndr_id_index ON cris.atd_txdot_person USING btree (prsn_gndr_id);
-CREATE INDEX atd_txdot_person_prsn_injry_sev_id_index ON cris.atd_txdot_person USING btree (prsn_injry_sev_id);
-CREATE INDEX atd_txdot_person_sus_serious_injry_cnt_index ON cris.atd_txdot_person USING btree (sus_serious_injry_cnt);
-CREATE INDEX idx_atd_txdot_person_crash_id ON cris.atd_txdot_person USING btree (crash_id);
 
 --ALTER TABLE ONLY cris.atd_txdot_person
     --ADD CONSTRAINT atd_txdot_person_prsn_injry_sev_id_fkey FOREIGN KEY (prsn_injry_sev_id) REFERENCES cris.atd_txdot__injry_sev_lkp(injry_sev_id) ON UPDATE RESTRICT ON DELETE RESTRICT;
@@ -384,17 +354,7 @@ ALTER TABLE ONLY cris.atd_txdot_primaryperson ALTER COLUMN primaryperson_id SET 
 ALTER TABLE ONLY cris.atd_txdot_primaryperson
     ADD CONSTRAINT atd_txdot_primaryperson_unique UNIQUE (crash_id, unit_nbr, prsn_nbr, prsn_type_id, prsn_occpnt_pos_id);
 
-CREATE INDEX atd_txdot_primaryperson_death_cnt_index ON cris.atd_txdot_primaryperson USING btree (death_cnt);
-CREATE INDEX atd_txdot_primaryperson_is_retired_index ON cris.atd_txdot_primaryperson USING btree (is_retired);
-CREATE INDEX atd_txdot_primaryperson_primaryperson_id_index ON cris.atd_txdot_primaryperson USING btree (primaryperson_id);
-CREATE INDEX atd_txdot_primaryperson_prsn_age_index ON cris.atd_txdot_primaryperson USING btree (prsn_age);
-CREATE INDEX atd_txdot_primaryperson_prsn_death_date_index ON cris.atd_txdot_primaryperson USING btree (prsn_death_date);
-CREATE INDEX atd_txdot_primaryperson_prsn_death_time_index ON cris.atd_txdot_primaryperson USING btree (prsn_death_time);
-CREATE INDEX atd_txdot_primaryperson_prsn_ethnicity_id_index ON cris.atd_txdot_primaryperson USING btree (prsn_ethnicity_id);
-CREATE INDEX atd_txdot_primaryperson_prsn_gndr_id_index ON cris.atd_txdot_primaryperson USING btree (prsn_gndr_id);
-CREATE INDEX atd_txdot_primaryperson_prsn_injry_sev_id_index ON cris.atd_txdot_primaryperson USING btree (prsn_injry_sev_id);
-CREATE INDEX atd_txdot_primaryperson_sus_serious_injry_cnt_index ON cris.atd_txdot_primaryperson USING btree (sus_serious_injry_cnt);
-CREATE INDEX idx_atd_txdot_primaryperson_crash_id ON cris.atd_txdot_primaryperson USING btree (crash_id);
+
 
 --------------------------------------------------------------------------------
 
@@ -532,6 +492,55 @@ ALTER TABLE ONLY cris.atd_txdot_units ALTER COLUMN unit_id SET DEFAULT nextval('
 ALTER TABLE ONLY cris.atd_txdot_units
     ADD CONSTRAINT atd_txdot_units_unique UNIQUE (crash_id, unit_nbr);
 
+--------------------------------------------------------------------------------
+
+CREATE INDEX atd_txdot_crashes_apd_confirmed_fatality_index ON cris.atd_txdot_crashes USING btree (apd_confirmed_fatality);
+CREATE INDEX atd_txdot_crashes_austin_full_purpose_index ON cris.atd_txdot_crashes USING btree (austin_full_purpose);
+CREATE INDEX atd_txdot_crashes_case_id_index ON cris.atd_txdot_crashes USING btree (case_id);
+CREATE INDEX atd_txdot_crashes_city_id_index ON cris.atd_txdot_crashes USING btree (city_id);
+CREATE INDEX atd_txdot_crashes_cr3_file_metadata_index ON cris.atd_txdot_crashes USING gin (cr3_file_metadata);
+CREATE INDEX atd_txdot_crashes_cr3_stored_flag_index ON cris.atd_txdot_crashes USING btree (cr3_stored_flag);
+CREATE INDEX atd_txdot_crashes_crash_date_index ON cris.atd_txdot_crashes USING btree (crash_date);
+CREATE INDEX atd_txdot_crashes_crash_fatal_fl_index ON cris.atd_txdot_crashes USING btree (crash_fatal_fl);
+CREATE INDEX atd_txdot_crashes_death_cnt_index ON cris.atd_txdot_crashes USING btree (death_cnt);
+CREATE INDEX atd_txdot_crashes_geocode_provider_index ON cris.atd_txdot_crashes USING btree (geocode_provider);
+CREATE INDEX atd_txdot_crashes_geocode_status_index ON cris.atd_txdot_crashes USING btree (geocode_status);
+CREATE INDEX atd_txdot_crashes_geocoded_index ON cris.atd_txdot_crashes USING btree (geocoded);
+CREATE INDEX atd_txdot_crashes_investigat_agency_id_index ON cris.atd_txdot_crashes USING btree (investigat_agency_id);
+CREATE INDEX atd_txdot_crashes_is_retired_index ON cris.atd_txdot_crashes USING btree (is_retired);
+CREATE INDEX atd_txdot_crashes_original_city_id_index ON cris.atd_txdot_crashes USING btree (original_city_id);
+CREATE INDEX atd_txdot_crashes_position_index ON cris.atd_txdot_crashes USING gist ("position");
+CREATE INDEX atd_txdot_crashes_qa_status_index ON cris.atd_txdot_crashes USING btree (qa_status);
+CREATE INDEX atd_txdot_crashes_sus_serious_injry_cnt_index ON cris.atd_txdot_crashes USING btree (sus_serious_injry_cnt);
+CREATE INDEX atd_txdot_crashes_temp_record_index ON cris.atd_txdot_crashes USING btree (temp_record);
+
+
+CREATE INDEX atd_txdot_person_death_cnt_index ON cris.atd_txdot_person USING btree (death_cnt);
+CREATE INDEX atd_txdot_person_is_retired_index ON cris.atd_txdot_person USING btree (is_retired);
+CREATE INDEX atd_txdot_person_person_id_index ON cris.atd_txdot_person USING btree (person_id);
+CREATE INDEX atd_txdot_person_prsn_age_index ON cris.atd_txdot_person USING btree (prsn_age);
+CREATE INDEX atd_txdot_person_prsn_death_date_index ON cris.atd_txdot_person USING btree (prsn_death_date);
+CREATE INDEX atd_txdot_person_prsn_death_time_index ON cris.atd_txdot_person USING btree (prsn_death_time);
+CREATE INDEX atd_txdot_person_prsn_ethnicity_id_index ON cris.atd_txdot_person USING btree (prsn_ethnicity_id);
+CREATE INDEX atd_txdot_person_prsn_gndr_id_index ON cris.atd_txdot_person USING btree (prsn_gndr_id);
+CREATE INDEX atd_txdot_person_prsn_injry_sev_id_index ON cris.atd_txdot_person USING btree (prsn_injry_sev_id);
+CREATE INDEX atd_txdot_person_sus_serious_injry_cnt_index ON cris.atd_txdot_person USING btree (sus_serious_injry_cnt);
+CREATE INDEX idx_atd_txdot_person_crash_id ON cris.atd_txdot_person USING btree (crash_id);
+
+
+CREATE INDEX atd_txdot_primaryperson_death_cnt_index ON cris.atd_txdot_primaryperson USING btree (death_cnt);
+CREATE INDEX atd_txdot_primaryperson_is_retired_index ON cris.atd_txdot_primaryperson USING btree (is_retired);
+CREATE INDEX atd_txdot_primaryperson_primaryperson_id_index ON cris.atd_txdot_primaryperson USING btree (primaryperson_id);
+CREATE INDEX atd_txdot_primaryperson_prsn_age_index ON cris.atd_txdot_primaryperson USING btree (prsn_age);
+CREATE INDEX atd_txdot_primaryperson_prsn_death_date_index ON cris.atd_txdot_primaryperson USING btree (prsn_death_date);
+CREATE INDEX atd_txdot_primaryperson_prsn_death_time_index ON cris.atd_txdot_primaryperson USING btree (prsn_death_time);
+CREATE INDEX atd_txdot_primaryperson_prsn_ethnicity_id_index ON cris.atd_txdot_primaryperson USING btree (prsn_ethnicity_id);
+CREATE INDEX atd_txdot_primaryperson_prsn_gndr_id_index ON cris.atd_txdot_primaryperson USING btree (prsn_gndr_id);
+CREATE INDEX atd_txdot_primaryperson_prsn_injry_sev_id_index ON cris.atd_txdot_primaryperson USING btree (prsn_injry_sev_id);
+CREATE INDEX atd_txdot_primaryperson_sus_serious_injry_cnt_index ON cris.atd_txdot_primaryperson USING btree (sus_serious_injry_cnt);
+CREATE INDEX idx_atd_txdot_primaryperson_crash_id ON cris.atd_txdot_primaryperson USING btree (crash_id);
+
+
 CREATE INDEX atd_txdot_units_death_cnt_index ON cris.atd_txdot_units USING btree (death_cnt);
 CREATE INDEX atd_txdot_units_movement_id_index ON cris.atd_txdot_units USING btree (movement_id);
 CREATE INDEX atd_txdot_units_sus_serious_injry_cnt_index ON cris.atd_txdot_units USING btree (sus_serious_injry_cnt);
@@ -539,26 +548,3 @@ CREATE INDEX atd_txdot_units_unit_id_index ON cris.atd_txdot_units USING btree (
 CREATE INDEX idx_atd_txdot_units_crash_id ON cris.atd_txdot_units USING btree (crash_id);
 
 
-
-
---------------------------------------------------------------------------------
-
- -- still working on how to handle these
-
-
-
-CREATE TRIGGER atd_txdot_crashes_audit_log BEFORE INSERT OR UPDATE ON cris.atd_txdot_crashes FOR EACH ROW EXECUTE FUNCTION cris.atd_txdot_crashes_updates_audit_log();
-
-CREATE TRIGGER atd_txdot_person_audit_log BEFORE UPDATE ON cris.atd_txdot_person FOR EACH ROW EXECUTE FUNCTION cris.atd_txdot_person_updates_audit_log();
-ALTER TABLE cris.atd_txdot_person DISABLE TRIGGER atd_txdot_person_audit_log;
-
-CREATE TRIGGER atd_txdot_primaryperson_audit_log BEFORE UPDATE ON cris.atd_txdot_primaryperson FOR EACH ROW EXECUTE FUNCTION cris.atd_txdot_primaryperson_updates_audit_log();
-
-CREATE TRIGGER atd_txdot_units_audit_log BEFORE UPDATE ON cris.atd_txdot_units FOR EACH ROW EXECUTE FUNCTION cris.atd_txdot_units_updates_audit_log();
-CREATE TRIGGER atd_txdot_units_create BEFORE INSERT ON cris.atd_txdot_units FOR EACH ROW EXECUTE FUNCTION cris.atd_txdot_units_create();
-CREATE TRIGGER atd_txdot_units_create_update BEFORE INSERT OR UPDATE ON cris.atd_txdot_units FOR EACH ROW EXECUTE FUNCTION cris.atd_txdot_units_create_update();
-
-CREATE TRIGGER "notify_hasura_jurisdiction_from_latlon_INSERT" AFTER INSERT ON cris.atd_txdot_crashes FOR EACH ROW EXECUTE FUNCTION hdb_views."notify_hasura_jurisdiction_from_latlon_INSERT"();
-CREATE TRIGGER "notify_hasura_jurisdiction_from_latlon_UPDATE" AFTER UPDATE ON cris.atd_txdot_crashes FOR EACH ROW EXECUTE FUNCTION hdb_views."notify_hasura_jurisdiction_from_latlon_UPDATE"();
-CREATE TRIGGER "notify_hasura_location_from_latlon_INSERT" AFTER INSERT ON cris.atd_txdot_crashes FOR EACH ROW EXECUTE FUNCTION hdb_views."notify_hasura_location_from_latlon_INSERT"();
-CREATE TRIGGER "notify_hasura_location_from_latlon_UPDATE" AFTER UPDATE ON cris.atd_txdot_crashes FOR EACH ROW EXECUTE FUNCTION hdb_views."notify_hasura_location_from_latlon_UPDATE"();
