26e007d2e1bbe15b9b98f9c1a0be97cd47468972
Frank Hereford <frankhereford@users.noreply.github.com>
2023-03-07 11:10:30 -0600

    make units view

--- atd-toolbox/materials_from_ldm_conversion/make_views.py
+++ atd-toolbox/materials_from_ldm_conversion/make_views.py
@@ -17,6 +17,7 @@ DB_SSL_REQUIREMENT = os.getenv("DB_SSL_REQUIREMENT")
 def main():
     create_schema()
     make_crashes_view()
+    make_units_view()
 
 def make_crashes_view():
     pg = get_pg_connection()
@@ -82,6 +83,78 @@ def make_crashes_view():
         from vz.atd_txdot_crashes
         join cris.atd_txdot_crashes
             on vz.atd_txdot_crashes.crash_id = cris.atd_txdot_crashes.crash_id
+                """
+    print(view)
+    db.execute(view)
+    pg.commit()
+
+def make_units_view():
+    pg = get_pg_connection()
+    db = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    db.execute("drop view if exists ldm.atd_txdot_units;")
+    pg.commit()
+
+
+    sql = """
+    with vz as (
+        select
+            'vz' as schema, ordinal_position,
+            column_name, data_type, udt_name, character_maximum_length, numeric_precision,
+            numeric_precision, numeric_scale, is_generated, generation_expression, is_updatable
+        FROM information_schema.columns
+        WHERE true
+            AND table_schema = 'vz'
+            AND table_name = 'atd_txdot_units'
+        order by ordinal_position
+        ), cris as (
+        SELECT
+            'cris' as schema, ordinal_position,
+            column_name, data_type, udt_name, character_maximum_length, numeric_precision,
+            numeric_precision, numeric_scale, is_generated, generation_expression, is_updatable
+        FROM information_schema.columns
+        WHERE true
+            AND table_schema = 'cris'
+            AND table_name = 'atd_txdot_units'
+        order by ordinal_position
+        )
+    select vz.column_name as vz_column_name, cris.column_name as cris_column_name,
+        case when vz.column_name is not null then vz.schema
+            when cris.column_name is not null then cris.schema
+        end as schema,
+        case when vz.column_name is not null then vz.column_name
+            when cris.column_name is not null then cris.column_name
+        end as column_name
+    from vz
+        full outer join cris on (vz.column_name = cris.column_name)
+    where true 
+        and (vz.column_name not in ('crash_id') or cris.column_name not in ('crash_id'))
+    order by coalesce(vz.ordinal_position, cris.ordinal_position)
+    """
+    db.execute(sql)
+
+
+    view = """
+    create view ldm.atd_txdot_units as
+        select
+            cris.atd_txdot_units.crash_id as crash_id,
+        """
+    columns = []
+    for column in db:
+        if column["vz_column_name"] is not None and column["cris_column_name"] is None:
+            columns.append(f'vz.atd_txdot_units.{column["column_name"]}')
+        elif column["cris_column_name"] is not None and column["vz_column_name"] is None:
+            columns.append(f'cris.atd_txdot_units.{column["column_name"]}')
+        else:
+            columns.append(f'coalesce(vz.atd_txdot_units.{column["column_name"]}, cris.atd_txdot_units.{column["column_name"]}) as {column["column_name"]}')
+    view = view + "    " + ", \n            ".join(columns)
+    view = view + """
+        from vz.atd_txdot_units
+        join cris.atd_txdot_units
+            on  (
+                    vz.atd_txdot_units.crash_id = cris.atd_txdot_units.crash_id
+                and vz.atd_txdot_units.unit_nbr = cris.atd_txdot_units.unit_nbr
+                )
                 """
     print(view)
     db.execute(view)
