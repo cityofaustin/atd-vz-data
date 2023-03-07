2285e3c00bbaaffa77dfa28d68a0906571c2acfa
Frank Hereford <frankhereford@users.noreply.github.com>
2023-03-07 11:35:40 -0600

    views for persons primary persons tables

--- atd-toolbox/materials_from_ldm_conversion/make_views.py
+++ atd-toolbox/materials_from_ldm_conversion/make_views.py
@@ -18,6 +18,8 @@ def main():
     create_schema()
     make_crashes_view()
     make_units_view()
+    make_person_view()
+    make_primaryperson_view()
 
 def make_crashes_view():
     pg = get_pg_connection()
@@ -154,6 +156,152 @@ def make_units_view():
             on  (
                     vz.atd_txdot_units.crash_id = cris.atd_txdot_units.crash_id
                 and vz.atd_txdot_units.unit_nbr = cris.atd_txdot_units.unit_nbr
+                )
+                """
+    print(view)
+    db.execute(view)
+    pg.commit()
+
+
+def make_person_view():
+    pg = get_pg_connection()
+    db = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    db.execute("drop view if exists ldm.atd_txdot_person;")
+    pg.commit()
+
+
+    sql = """
+    with vz as (
+        select
+            'vz' as schema, ordinal_position,
+            column_name, data_type, udt_name, character_maximum_length, numeric_precision,
+            numeric_precision, numeric_scale, is_generated, generation_expression, is_updatable
+        FROM information_schema.columns
+        WHERE true
+            AND table_schema = 'vz'
+            AND table_name = 'atd_txdot_person'
+        order by ordinal_position
+        ), cris as (
+        SELECT
+            'cris' as schema, ordinal_position,
+            column_name, data_type, udt_name, character_maximum_length, numeric_precision,
+            numeric_precision, numeric_scale, is_generated, generation_expression, is_updatable
+        FROM information_schema.columns
+        WHERE true
+            AND table_schema = 'cris'
+            AND table_name = 'atd_txdot_person'
+        order by ordinal_position
+        )
+    select vz.column_name as vz_column_name, cris.column_name as cris_column_name,
+        case when vz.column_name is not null then vz.schema
+            when cris.column_name is not null then cris.schema
+        end as schema,
+        case when vz.column_name is not null then vz.column_name
+            when cris.column_name is not null then cris.column_name
+        end as column_name
+    from vz
+        full outer join cris on (vz.column_name = cris.column_name)
+    where true 
+        and (vz.column_name not in ('crash_id') or cris.column_name not in ('crash_id'))
+    order by coalesce(vz.ordinal_position, cris.ordinal_position)
+    """
+    db.execute(sql)
+
+
+    view = """
+    create view ldm.atd_txdot_person as
+        select
+            cris.atd_txdot_person.crash_id as crash_id,
+        """
+    columns = []
+    for column in db:
+        if column["vz_column_name"] is not None and column["cris_column_name"] is None:
+            columns.append(f'vz.atd_txdot_person.{column["column_name"]}')
+        elif column["cris_column_name"] is not None and column["vz_column_name"] is None:
+            columns.append(f'cris.atd_txdot_person.{column["column_name"]}')
+        else:
+            columns.append(f'coalesce(vz.atd_txdot_person.{column["column_name"]}, cris.atd_txdot_person.{column["column_name"]}) as {column["column_name"]}')
+    view = view + "    " + ", \n            ".join(columns)
+    view = view + """
+        from vz.atd_txdot_person
+        join cris.atd_txdot_person
+            on  (
+                    vz.atd_txdot_person.crash_id = cris.atd_txdot_person.crash_id
+                and vz.atd_txdot_person.unit_nbr = cris.atd_txdot_person.unit_nbr
+                )
+                """
+    print(view)
+    db.execute(view)
+    pg.commit()
+
+
+def make_primaryperson_view():
+    pg = get_pg_connection()
+    db = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    db.execute("drop view if exists ldm.atd_txdot_primaryperson;")
+    pg.commit()
+
+
+    sql = """
+    with vz as (
+        select
+            'vz' as schema, ordinal_position,
+            column_name, data_type, udt_name, character_maximum_length, numeric_precision,
+            numeric_precision, numeric_scale, is_generated, generation_expression, is_updatable
+        FROM information_schema.columns
+        WHERE true
+            AND table_schema = 'vz'
+            AND table_name = 'atd_txdot_primaryperson'
+        order by ordinal_position
+        ), cris as (
+        SELECT
+            'cris' as schema, ordinal_position,
+            column_name, data_type, udt_name, character_maximum_length, numeric_precision,
+            numeric_precision, numeric_scale, is_generated, generation_expression, is_updatable
+        FROM information_schema.columns
+        WHERE true
+            AND table_schema = 'cris'
+            AND table_name = 'atd_txdot_primaryperson'
+        order by ordinal_position
+        )
+    select vz.column_name as vz_column_name, cris.column_name as cris_column_name,
+        case when vz.column_name is not null then vz.schema
+            when cris.column_name is not null then cris.schema
+        end as schema,
+        case when vz.column_name is not null then vz.column_name
+            when cris.column_name is not null then cris.column_name
+        end as column_name
+    from vz
+        full outer join cris on (vz.column_name = cris.column_name)
+    where true 
+        and (vz.column_name not in ('crash_id') or cris.column_name not in ('crash_id'))
+    order by coalesce(vz.ordinal_position, cris.ordinal_position)
+    """
+    db.execute(sql)
+
+
+    view = """
+    create view ldm.atd_txdot_primaryperson as
+        select
+            cris.atd_txdot_primaryperson.crash_id as crash_id,
+        """
+    columns = []
+    for column in db:
+        if column["vz_column_name"] is not None and column["cris_column_name"] is None:
+            columns.append(f'vz.atd_txdot_primaryperson.{column["column_name"]}')
+        elif column["cris_column_name"] is not None and column["vz_column_name"] is None:
+            columns.append(f'cris.atd_txdot_primaryperson.{column["column_name"]}')
+        else:
+            columns.append(f'coalesce(vz.atd_txdot_primaryperson.{column["column_name"]}, cris.atd_txdot_primaryperson.{column["column_name"]}) as {column["column_name"]}')
+    view = view + "    " + ", \n            ".join(columns)
+    view = view + """
+        from vz.atd_txdot_primaryperson
+        join cris.atd_txdot_primaryperson
+            on  (
+                    vz.atd_txdot_primaryperson.crash_id = cris.atd_txdot_primaryperson.crash_id
+                and vz.atd_txdot_primaryperson.unit_nbr = cris.atd_txdot_primaryperson.unit_nbr
                 )
                 """
     print(view)
