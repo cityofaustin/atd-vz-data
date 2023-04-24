c1d0cf115f6182eabd503c94e89aa44561d7a487
Frank Hereford <frank@frankhereford.com>
2023-04-24 09:46:14 -0500

    persons table trigger

--- atd-toolbox/materials_from_ldm_conversion/create_cris_triggers_to_maintain_vz.sql
+++ atd-toolbox/materials_from_ldm_conversion/create_cris_triggers_to_maintain_vz.sql
@@ -33,3 +33,22 @@ AFTER INSERT OR DELETE
 ON cris.atd_txdot_units
 FOR EACH ROW
 EXECUTE FUNCTION maintain_vz_schema_unit_records();
+
+
+
+CREATE OR REPLACE FUNCTION maintain_vz_schema_person_records()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF TG_OP = 'INSERT' OR TG_OP = 'DELETE' THEN
+    INSERT INTO vz.atd_txdot_person (crash_id, unit_nbr, prsn_nbr) VALUES (NEW.crash_id, NEW.unit_nbr, NEW.prsn_nbr);
+  END IF;
+  RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trg_vz_create_atd_txdot_person
+AFTER INSERT OR DELETE
+ON cris.atd_txdot_person
+FOR EACH ROW
+EXECUTE FUNCTION maintain_vz_schema_person_records();
