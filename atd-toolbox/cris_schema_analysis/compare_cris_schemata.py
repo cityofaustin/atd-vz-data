a9df3c8c8dea9cf4da5ad931c3f7ac139c997995
Frank Hereford <frankhereford@users.noreply.github.com>
2023-08-24 16:58:15 -0500

    comparison script

--- atd-toolbox/cris_schema_analysis/compare_cris_schemata.py
+++ atd-toolbox/cris_schema_analysis/compare_cris_schemata.py
@@ -1,61 +1,67 @@
-import argparse
 import csv
-import json
+import argparse
 from collections import defaultdict
 
-def main():
-    parser = argparse.ArgumentParser(description='Compare two database schema CSV files.')
-    parser.add_argument('--first-schema', type=str, required=True, help='Path to the first schema CSV file.')
-    parser.add_argument('--second-schema', type=str, required=True, help='Path to the second schema CSV file.')
+def read_csv(file_path):
+    groups = defaultdict(list)
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skipping the header
+        for row in reader:
+            column_name, record_id, description = row
+            groups[column_name].append((record_id, description))
+    return groups
 
+def compare_schemas(first_schema, second_schema):
+    additions = defaultdict(list)
+    removals = defaultdict(list)
+    all_column_names = set(first_schema.keys()) | set(second_schema.keys())
+    for column_name in all_column_names:
+        first_records = set(record[0] for record in first_schema.get(column_name, []))
+        second_records = set(record[0] for record in second_schema.get(column_name, []))
+        for added_id in second_records - first_records:
+            added_record = next((record for record in second_schema[column_name] if record[0] == added_id), None)
+            if added_record:
+                additions[column_name].append(added_record)
+        for removed_id in first_records - second_records:
+            removed_record = next((record for record in first_schema[column_name] if record[0] == removed_id), None)
+            if removed_record:
+                removals[column_name].append(removed_record)
+    return additions, removals
+
+def print_report(additions, removals):
+    print("Report:")
+    print("=======\n")
+    print("Additions:")
+    for column_name, records in additions.items():
+        if records:
+            print(f"- {column_name}:")
+            for record in records:
+                print(f"  ID: {record[0]}, Description: {record[1]}")
+        else:
+            print(f"- {column_name}: None")
+    print()
+    print("Removals:")
+    for column_name, records in removals.items():
+        if records:
+            print(f"- {column_name}:")
+            for record in records:
+                print(f"  ID: {record[0]}, Description: {record[1]}")
+        else:
+            print(f"- {column_name}: None")
+    print()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--first-schema', required=True, help='Path to the first schema file')
+    parser.add_argument('--second-schema', required=True, help='Path to the second schema file')
     args = parser.parse_args()
 
-    first_schema = args.first_schema
-    second_schema = args.second_schema
+    first_schema = read_csv(args.first_schema)
+    second_schema = read_csv(args.second_schema)
 
-    # Read the first schema CSV file
-    with open(first_schema, 'r') as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header
-        first_schema_data = defaultdict(list)
-        for row in reader:
-            first_schema_data[row[0]].append({row[1]: row[2]})
-
-    # Read the second schema CSV file
-    with open(second_schema, 'r') as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header
-        second_schema_data = defaultdict(list)
-        for row in reader:
-            second_schema_data[row[0]].append({row[1]: row[2]})
-
-    # Compare the two schemas and produce a report
-    for column_name in set(first_schema_data.keys()).union(second_schema_data.keys()):
-        first_schema_records = first_schema_data[column_name]
-        second_schema_records = second_schema_data[column_name]
-
-        removed_records = [record for record in first_schema_records if record not in second_schema_records]
-        added_records = [record for record in second_schema_records if record not in first_schema_records]
-
-        print("Removed records:", removed_records)
-
-        if removed_records or added_records:
-            print(f"Changes for {column_name}:")
-
-        if added_records:
-            print("Additions:")
-            for record in added_records:
-                for id, description in record.items():
-                    print(f"{id}: {description}")
-
-        if removed_records:
-            print("Removals:")
-            for record in removed_records:
-                for id, description in record.items():
-                    print(f"{id}: {description}")
-
-        if removed_records or added_records:
-            print()
+    additions, removals = compare_schemas(first_schema, second_schema)
+    print_report(additions, removals)
 
 if __name__ == "__main__":
     main()
